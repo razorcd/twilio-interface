d7d7965bcc534aff41c5655b24938723774c7fc9
razor <razorcd@yahoo.com>
2016-05-23 12:25:37 +0200

    adds strong params to routes

--- app/routes.rb
+++ app/routes.rb
@@ -5,14 +5,9 @@ get '/' do
 end
 
 post '/send_message' do
-  # TODO: add strong params
-  # params['account_id']= "XXXX"
-  # params['auth_id']= "xxxx"
-  # params['from_number']= "111"
-  # params['to_number']= "111"
-  # params['body']= "1234567"
-  successful= Messanger.new(account_id: params['account_id'], auth_id: params['auth_id']).
-      send_message(from_number: params['from_number'], to_number: params['to_number'], body: params['body'])
+  parsed_params= parsed_send_message_params params
+  successful= Messanger.new(account_id: parsed_params[:account_id], auth_id: parsed_params[:auth_id]).
+      send_message(from_number: parsed_params[:from_number], to_number: parsed_params[:to_number], body: parsed_params[:body])
 
 
   headers "Content-Type" => "application/json"
@@ -25,9 +20,24 @@ post '/send_message' do
 end
 
 get '/list_messages' do
-  # TODO: add strong params
   headers "Content-Type" => "application/json"
-  # params['account_id']= "XXXX"
-  # params['auth_id']= "xxxx"
-  Messanger.new(account_id: params['account_id'], auth_id: params['auth_id']).list_messages
+  parsed_params= parsed_list_messages_params params
+  Messanger.new(account_id: parsed_params[:account_id], auth_id: parsed_params[:auth_id]).list_messages
+end
+
+def parsed_send_message_params params
+  {
+    account_id: params["account_id_clone"],
+    auth_id: params["auth_id_clone"],
+    from_number: params["from_number"],
+    to_number: params["to_number"],
+    body: params["body"],
+  }
+end
+
+def parsed_list_messages_params params
+  {
+    account_id: params["account_id_clone"],
+    auth_id: params["auth_id_clone"],
+  }
 end
