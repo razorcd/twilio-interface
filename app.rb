349256441e4102315f9d52eeacfc9c29f1622d68
razor <razorcd@yahoo.com>
2016-05-26 00:40:33 +0200

    moves sinatra configs in own file

--- app.rb
+++ app.rb
@@ -1,9 +1,6 @@
 require 'sinatra'
 
-set :app_file, __FILE__
-set :root, File.dirname(__FILE__)
-set :public_folder, 'public'
-set :views, 'app/views'
+require "./config/application"
 
 require "./app/helpers"
 helpers Helpers
---
+++ config/application.rb
@@ -0,0 +1,4 @@
+set :app_file, __FILE__
+set :root, File.dirname(__FILE__)
+set :public_folder, 'public'
+set :views, 'app/views'
