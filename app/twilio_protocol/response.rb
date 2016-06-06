de5dbd4b8d4a2c8841e6f290d8d21958bdd789ff
razor <razorcd@yahoo.com>
2016-06-06 13:58:26 +0200

    twilio protocol returns serialised Response instance

--- app/twilio_protocol/response.rb
+++ app/twilio_protocol/response.rb
@@ -1,28 +1,30 @@
 require 'json'
 
-class TwilioProtocol::Response
-  def initialize response
-    @response= response
+class Response
+  attr_reader :response_body
+
+  def initialize response_body
+    @response_body= response_body
   end
 
   def successful?
-    status= json_response["status"]
+    status= json_response_body["status"]
     return false if status && status.to_s[0] != 2
     true
   end
 
   def messages
-    json_response["messages"]
+    json_response_body["messages"]
   end
 
   def error_message
-    json_response["message"]
+    json_response_body["message"]
   end
 
 private
 
-  def json_response
-    JSON.parse(@response)
+  def json_response_body
+    JSON.parse(response_body)
   end
 
 end
