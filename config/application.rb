3a6f29fe509c53bfb4273174d3e88144337127ed
razor <razorcd@yahoo.com>
2016-06-08 20:12:50 +0200

    sets env var at heroku start

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,9 @@
 require_relative "version"
+require 'logger'
 
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :public_folder, 'public'
 set :views, 'app/views'
+set :logger, Logger.new(STDOUT) if ENV['RACK_ENV']!="production"
 set :version, Version::VERSION
